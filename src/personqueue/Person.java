b76ed07f29a5c7b5eec4b2e8c4353c871d913c09
Tony <tananh691@gmail.com>
2021-01-22 09:13:48 +0700

    change person class

--- src/personqueue/Person.java
+++ src/personqueue/Person.java
@@ -17,15 +17,7 @@ public class Person {
     public Person(String name){
         this.name=name;
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
+    
     @Override
     public String toString(){
         return name;
